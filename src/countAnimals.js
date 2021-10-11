5027a1c9e641f9c3dc818edb05841c631edbdeed
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-11 18:15:11 -0300

    adicionado requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, current) => {
+      acc[current.name] = current.residents.length;
+      return acc;
+    }, {});
+  }
+
+  if (animal.sex !== undefined) {
+    return data.species.find((specie) =>
+      specie.name.includes(animal.specie)).residents.filter((specieSex) =>
+      specieSex.sex === animal.sex).length;
+  } return data.species.find((specie) =>
+    specie.name.includes(animal.specie)).residents.length;
 }
+
+// console.log(countAnimals());
 
 module.exports = countAnimals;
