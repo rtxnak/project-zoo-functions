b8d2ba67b2f209efebab5f9df9657e9ac18dd345
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-13 14:46:46 -0300

    correcao requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => (employee.managers.includes(id)));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const results = data.employees.filter((employee) =>
+    employee.managers.includes(managerId));
+  return results.map((result) => `${result.firstName} ${result.lastName}`);
 }
-
 module.exports = { isManager, getRelatedEmployees };
