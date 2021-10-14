909702ba02e7d3948a5bea7e57de8cc9a4e580ea
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-14 17:40:02 -0300

    adicionado requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,64 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function getEmployeesInfo(employeeInfo) {
+  const firstNameRe = data.employees.find((employee) => employee.firstName === employeeInfo.name);
+  const LastNameRe = data.employees.find((employee) => employee.lastName === employeeInfo.name);
+  const idResult = data.employees.find((employee) => employee.id === employeeInfo.id);
+  if (firstNameRe !== undefined) {
+    return firstNameRe;
+  }
+  if (LastNameRe !== undefined) {
+    return LastNameRe;
+  }
+  if (idResult !== undefined) {
+    return idResult;
+  }
+}
+
+function getEmployees(option) {
+  const employeeInfo = getEmployeesInfo(option);
+  if (employeeInfo === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const speciesInfo = data.species.filter((specie) =>
+    employeeInfo.responsibleFor.includes(specie.id));
+
+  return {
+    id: employeeInfo.id,
+    fullName: `${employeeInfo.firstName} ${employeeInfo.lastName}`,
+    species: speciesInfo.map((specie) => specie.name),
+    locations: speciesInfo.map((specie) => specie.location),
+  };
+}
+
+function getAllEmployees() {
+  const allEmployees = employees.map((employee) => ({ name: employee.firstName }));
+  return allEmployees.map((employee) => (getEmployees(employee)));
+}
+
+function getEmployeesCoverage(option = 'default') {
+  if (option === 'default') {
+    return getAllEmployees();
+  }
+  return getEmployees(option);
 }
 
 module.exports = getEmployeesCoverage;
+
+/*
+ENTRADAS:
+getEmployeesCoverage({ name: 'Sharonda' });
+getEmployeesCoverage({ name: 'Spry' });
+getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' });
+
+SAIDAS:
+
+{
+  "id": "4b40a139-d4dc-4f09-822d-ec25e819a5ad",
+  "fullName": "Sharonda Spry",
+  "species": [ "otters", "frogs" ],
+  "locations": [ "SE", "SW" ]
+}
+
+*/
