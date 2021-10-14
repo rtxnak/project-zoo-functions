1b1a1974894c7a5635f3122b3f17f42fe83fe2c7
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-14 00:02:13 -0300

    adicionado requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,42 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const weekDays = Object.keys(data.hours);
+const animals = data.species.map(({ name }) => name);
+
+function showAllDay(userDay) {
+  const schedule = {};
+  weekDays.forEach((day) => {
+    schedule[day] = {};
+    schedule[day].officeHour = `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+    schedule[day].exhibition = data.species.filter((specie) =>
+      specie.availability.includes(day)).map((animalName) => animalName.name);
+    if (day === 'Monday') {
+      schedule[day].officeHour = 'CLOSED';
+      schedule[day].exhibition = 'The zoo will be closed!';
+    }
+  });
+  if (!userDay) {
+    return schedule;
+  } return { [userDay]: schedule[userDay] };
 }
+
+function showAnimalsPerWeekDay(userAnimal) {
+  return data.species.find((specie) =>
+    specie.name === userAnimal).availability;
+}
+
+function getSchedule(scheduleTarget) {
+  if (weekDays.includes(scheduleTarget)) {
+    return showAllDay(scheduleTarget);
+  }
+  if (animals.includes(scheduleTarget)) {
+    return showAnimalsPerWeekDay(scheduleTarget);
+  }
+  return showAllDay();
+}
+
+// console.log(getSchedule('qualquercoisa'));
+// console.log(showAnimalsPerWeekDay('penguins'));
 
 module.exports = getSchedule;
