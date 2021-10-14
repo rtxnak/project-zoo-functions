9b44046bd9015b1ee25523b87f59bf18c74c13c4
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2021-10-14 15:22:38 -0300

    adicionado requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeFind = employees.find((employee) => employee.id === id);
+  const aniamalResp = data.species.find((specie) => specie.id === employeeFind.responsibleFor[0]);
+  const result = aniamalResp.residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc));
+  return [result.name, result.sex, result.age];
 }
+
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
